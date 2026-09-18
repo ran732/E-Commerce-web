7e21a91d85653ca769992bb664a80b311dc033fe
ran702 <ajeetranjeet495@gmail.com>
2026-09-18 22:32:11 +0530

    Learning about grouping item

--- store/views.py
+++ store/views.py
@@ -53,13 +53,15 @@ def product_detail(request,category_slug,product_slug):
 
 def search(request):
     
-    if 'keyword' in request.GET:
+    if 'keyword' in request.GET:  #cheak data from request
         
-        keyword = request.GET['keyword']
+        keyword = request.GET['keyword']  #fetch data from request  in variable keyward (keyward itself a query)
         
         if keyword:
             products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            
             product_count = products.count()
+            
     context = {
         'products': products,
         'product_count' : product_count,
